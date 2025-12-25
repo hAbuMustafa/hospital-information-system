cb79967816d70ab48c9641dd18e603b534aa9eeb
hAbuMustafa <hosam5553@gmail.com>
2025-12-25 15:28:55 +0200

    add schema barrel to db obj

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { NODE_ENV, PSQL_CONNECTION_STRING } from '$env/static/private';
+import { Pool } from 'pg';
+import schema from './schema/entities';
 
-export const db = drizzle(
-  `${PSQL_CONNECTION_STRING}${NODE_ENV !== 'production' ? '_TEST' : ''}`
-);
+const pool = new Pool({
+  connectionString: `${PSQL_CONNECTION_STRING}${
+    NODE_ENV !== 'production' ? '_TEST' : ''
+  }`,
+});
+
+export const db = drizzle(pool, { schema });
