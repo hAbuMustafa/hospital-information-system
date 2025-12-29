407754a74cc2ffd58510545a99ff3363606f919b
hAbuMustafa <hosam5553@gmail.com>
2025-12-29 02:30:57 +0200

    fixed patient seed steps

--- src/lib/server/db/operations/seed/patients.ts
+++ src/lib/server/db/operations/seed/patients.ts
@@ -4,42 +4,67 @@ import {
   InPatient,
   Diagnosis,
   Patient_diagnosis,
+  Discharge,
 } from '$lib/server/db/schema/entities/patients';
-import { Person } from '$lib/server/db/schema/entities/people';
+import { Person, Person_IdDoc } from '$lib/server/db/schema/entities/people';
 import { verifyEgyptianNationalId } from '$lib/utils/id-number-validation/egyptian-national-id';
 import { eq } from 'drizzle-orm';
+import { createDiagnosis } from '$lib/server/db/operations/menus';
 
 export async function seedPatient(patient: App.CustomTypes['PatientSeedT']) {
   try {
     const new_patient = await db.transaction(async (tx) => {
-      let foundPerson: typeof Person.$inferSelect | null = null;
+      let foundPersonId: number | null = null;
 
-      if (!patient.person_id) {
-        if (patient.id_doc_num) {
-          [foundPerson] = await tx
-            .select()
-            .from(Person)
-            .where(eq(Person.id_doc_num, patient.id_doc_num!));
-        }
-
-        if (!foundPerson) {
-          let numberValidity;
-
-          if (patient.id_doc_type === 1 && patient.id_doc_num) {
-            try {
-              numberValidity = verifyEgyptianNationalId(patient.id_doc_num);
-              if (!numberValidity) patient.id_doc_num = patient.id_doc_num + ' INVALID';
-            } catch (e) {
-              patient.id_doc_num = patient.id_doc_num + ' INVALID';
-            }
-          }
-
-          const { id: droppedPatientId, ...restOfPatientData } = patient;
-          [foundPerson] = await tx.insert(Person).values(restOfPatientData).returning();
-        }
-
-        patient.person_id = foundPerson.id;
+      if (patient.id_doc_num) {
+        let [firstResult] = await tx
+          .select({ id: Person_IdDoc.person_id })
+          .from(Person_IdDoc)
+          .where(eq(Person_IdDoc.document_number, patient.id_doc_num));
+        foundPersonId = firstResult.id;
       }
+
+      if (!foundPersonId) {
+        let numberValidity;
+
+        if (patient.id_doc_type === 1 && patient.id_doc_num) {
+          try {
+            numberValidity = verifyEgyptianNationalId(patient.id_doc_num);
+            if (!numberValidity) patient.id_doc_num = patient.id_doc_num + ' INVALID';
+          } catch (e) {
+            patient.id_doc_num = patient.id_doc_num + ' INVALID';
+          }
+        }
+
+        const {
+          id: droppedPatientId,
+          name: patient_name,
+          ...restOfPatientData
+        } = patient;
+        const [first_name, father_name, grandfather_name, ...family_name] =
+          patient_name.split(' ');
+        let [newPersonInsert] = await tx
+          .insert(Person)
+          .values({
+            ...restOfPatientData,
+            first_name,
+            father_name,
+            grandfather_name,
+            family_name: family_name.join(' '),
+          })
+          .returning();
+        foundPersonId = newPersonInsert.id;
+
+        if (patient.id_doc_num) {
+          await tx.insert(Person_IdDoc).values({
+            person_id: foundPersonId,
+            document_type: patient.id_doc_type,
+            document_number: patient.id_doc_num,
+          });
+        }
+      }
+
+      patient.person_id = foundPersonId;
 
       if (patient.admission_notes?.includes('مسجون')) {
         patient.security_status = true;
@@ -49,17 +74,27 @@ export async function seedPatient(patient: App.CustomTypes['PatientSeedT']) {
       const [newPatient] = await tx
         .insert(InPatient)
         .values({
-          ...patient,
+          file_id: patient.id,
+          person_id: foundPersonId,
+          security_status: patient.security_status,
           recent_ward: patient.admission_ward,
-        } as App.Require<App.CustomTypes['PatientSeedT'], 'person_id'>)
+        })
         .returning();
 
       await tx.insert(Transfer).values({
         patient_id: newPatient.id,
-        ward: patient.admission_ward,
+        to_ward_id: patient.admission_ward,
         timestamp: patient.admission_date,
         notes: 'admission',
       });
+
+      if (patient.discharge_date && patient.discharge_reason) {
+        await tx.insert(Discharge).values({
+          patient_id: newPatient.id,
+          discharge_reason: patient.discharge_reason,
+          timestamp: patient.discharge_date,
+        });
+      }
 
       const pDiagnoses = patient.diagnosis.split('+').map((d) => d.trim());
 
@@ -86,7 +121,7 @@ export async function seedPatient(patient: App.CustomTypes['PatientSeedT']) {
               currentDiagnosisText,
               'for patient',
               newPatient.id,
-              foundPerson?.name
+              patient.name
             );
             continue;
           }
@@ -96,6 +131,7 @@ export async function seedPatient(patient: App.CustomTypes['PatientSeedT']) {
           patient_id: newPatient.id,
           diagnosis_id: diagnosis.id,
           timestamp: patient.admission_date,
+          type: 'Initial',
         });
       }
 
