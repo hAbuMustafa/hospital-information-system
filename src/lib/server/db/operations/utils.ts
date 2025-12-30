714c0bbeae9d0d153fbf54f0383a8393880c8739
hAbuMustafa <hosam5553@gmail.com>
2025-12-30 19:58:19 +0200

    specify not discharged

--- src/lib/server/db/operations/utils.ts
+++ src/lib/server/db/operations/utils.ts
@@ -1,18 +1,16 @@
 import { db } from '$lib/server/db';
-import {
-  Diagnosis,
-  current_inPatient_view,
-} from '$lib/server/db/schema/entities/patients';
-import { and, eq } from 'drizzle-orm';
+import { Diagnosis, inPatient_view } from '$lib/server/db/schema/entities/patients';
+import { and, eq, isNull } from 'drizzle-orm';
 
 export async function isAdmitted(idDocType: number, idDocNum: string) {
   const [foundAdmitted] = await db
     .select()
-    .from(current_inPatient_view)
+    .from(inPatient_view)
     .where(
       and(
-        eq(current_inPatient_view.id_doc_type, idDocType),
-        eq(current_inPatient_view.id_doc_number, idDocNum)
+        eq(inPatient_view.id_doc_type, idDocType),
+        eq(inPatient_view.id_doc_number, idDocNum),
+        isNull(inPatient_view.discharge_time)
       )
     );
   return foundAdmitted;
