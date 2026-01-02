01c4aa97fb2ece6029e5a13c1868d354d773dc8e
hAbuMustafa <hosam5553@gmail.com>
2026-01-02 15:22:55 +0200

    separate type

--- src/lib/server/db/operations/types.d.ts
+++ src/lib/server/db/operations/types.d.ts
@@ -11,3 +11,12 @@ export type newPatientT = {
   referred_from: string;
   person_id?: number;
 };
+
+export type NewUserDataT = {
+  username: string;
+  password: string;
+  name: string;
+  national_id: string;
+  email: string;
+  phone_number: string;
+};
