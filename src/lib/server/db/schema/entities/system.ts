5ae855b97f5c9873a58fd39f7f54ec548061b9ca
hAbuMustafa <hosam5553@gmail.com>
2026-01-05 14:21:08 +0200

    users view added

--- src/lib/server/db/schema/entities/system.ts
+++ src/lib/server/db/schema/entities/system.ts
@@ -9,9 +9,11 @@ import {
   timestamp,
   pgSchema,
   smallint,
+  date,
 } from 'drizzle-orm/pg-core';
 import { Person } from './people';
 import { Staff } from './hospital';
+import { sql } from 'drizzle-orm';
 
 export const Security = pgSchema('Security');
 
@@ -58,3 +60,58 @@ export const User = Security.table('User', {
   last_login: timestamp({ mode: 'date' }),
   password_reset_required: boolean().default(false).notNull(),
 });
+
+export const users_view = Security.view('users_view', {
+  user_id: integer(),
+  person_id: integer(),
+  first_name: varchar({ length: 45 }),
+  person_name: text(),
+  id_doc_type: varchar({ length: 16 }),
+  id_doc_num: varchar({ length: 45 }),
+  gender: boolean(),
+  birthdate: date(),
+  race: varchar({ length: 16 }),
+  marital_status: boolean(),
+  religion: varchar({ length: 15 }),
+  occupation: varchar({ length: 45 }),
+  contact_type: varchar({ length: 15 }),
+  contact_string: varchar({ length: 100 }),
+  username: varchar({ length: 45 }).notNull().unique(),
+  hashed_pw: text().notNull(),
+  role: integer().notNull(),
+  staff_id: smallint().references(() => Staff.id),
+  created_at: timestamp({ mode: 'date' }).defaultNow().notNull(),
+  active: boolean().default(false).notNull(),
+  last_login: timestamp({ mode: 'date' }),
+  password_reset_required: boolean().default(false).notNull(),
+}).as(sql`
+SELECT 
+  u.id AS user_id,
+  u.person_id,
+  pr.first_name,
+  CONCAT_WS(' ', pr.first_name, pr.father_name, pr.grandfather_name, pr.family_name) as "person_name",
+  doctype.name as id_doc_type,
+	doc.document_number as id_doc_number,
+	pr.gender,
+	pr.birthdate,
+	pr.race,
+	pr.marital_status,
+	pr.religion,
+	pr.occupation,
+	ct.name as contact_type,
+	con.contact_string,
+  u.username,
+  u.hashed_pw,
+  u.role,
+  u.staff_id,
+  u.created_at,
+  u.active,
+  u.last_login,
+  u.password_reset_required
+FROM "Security"."Users" u
+  LEFT JOIN "People"."Person" pr ON u.person_id = pr.id
+  LEFT JOIN "People"."Person_IdDoc" doc on doc.person_id = pr.id
+  LEFT JOIN "People"."IdDoc_type" doctype on doc.document_type = doctype.id
+  LEFT JOIN "People"."People_contact_information" con on con.person_id = pr.id
+  LEFT JOIN "People"."Contact_type" ct on con.contact_type = ct.id
+  `);
