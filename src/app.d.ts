0c534c6853b86b7c70a409b83a7bcb0df2242c11
hAbuMustafa <hosam5553@gmail.com>
2026-01-07 14:22:41 +0200

    general typing

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,23 +1,13 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { users_view } from '$lib/server/db/schema/entities/system';
+
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
     interface Locals {
-      user: {
-        id: number;
-        username: string;
-        name: string;
-        first_name: string;
-        phone_number: string;
-        email: string | null;
-        national_id: string | null;
-        role: number;
-        created_at: Date;
-        last_login: Date | null;
-        password_reset_required?: boolean;
-        gravatar: string;
-      } | null;
+      user: typeof users_view.$inferSelect | null;
     }
     // interface PageData {}
     // interface PageState {}
