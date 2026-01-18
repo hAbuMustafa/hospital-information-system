4497824d1e75dbb6f1dbe192cb8a986ab6677578
hAbuMustafa <hosam5553@gmail.com>
2026-01-18 13:38:41 +0200

    exclude selected admission

--- src/routes/(authed)/api/patients/[patient_id]/admissions/+server.ts
+++ src/routes/(authed)/api/patients/[patient_id]/admissions/+server.ts
@@ -1,18 +1,20 @@
 import { db } from '$server/db/index.js';
 import { inPatient_view } from '$server/db/schema/entities/patients';
 import { json } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import { and, eq, ne } from 'drizzle-orm';
 
-export async function GET({ url }) {
+export async function GET({ params, url }) {
   const person_query = url.searchParams.get('person_id');
 
   if (person_query && !/\d+/.test(person_query))
     return new Response('Bad request', { status: 401 });
 
   let person_id;
+  let patient_id;
 
   try {
     person_id = Number(person_query);
+    patient_id = Number(params.patient_id);
   } catch (error) {
     console.error(error);
     return new Response('Bad request', { status: 401 });
@@ -21,7 +23,12 @@ export async function GET({ url }) {
   const diagnoses = await db
     .select()
     .from(inPatient_view)
-    .where(eq(inPatient_view.person_id, person_id));
+    .where(
+      and(
+        eq(inPatient_view.person_id, person_id),
+        ne(inPatient_view.patient_id, patient_id)
+      )
+    );
 
   return json(diagnoses);
 }
