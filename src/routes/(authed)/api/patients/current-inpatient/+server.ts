b59fd0e85237fb2315e57ca86c8f715c981b5d5e
hAbuMustafa <hosam5553@gmail.com>
2026-01-19 21:03:51 +0200

    comply with new data structure

--- src/routes/(authed)/api/patients/current-inpatient/+server.ts
+++ src/routes/(authed)/api/patients/current-inpatient/+server.ts
@@ -1,34 +1,12 @@
 import { db } from '$lib/server/db';
-import {
-  Diagnoses,
-  Patient_Diagnoses,
-  Patients,
-  People,
-  Wards,
-} from '$lib/server/db/schema/entities';
+import { inPatient_view } from '$server/db/schema/entities/patients';
 import { json } from '@sveltejs/kit';
-import { eq, isNull, sql } from 'drizzle-orm';
+import { isNull } from 'drizzle-orm';
 
 export async function GET() {
   const allPatients = await db
-    .select({
-      id: Patients.id,
-      name: People.name,
-      admission_date: Patients.admission_date,
-      recent_ward: Patients.recent_ward,
-      diagnosis: sql<string>`COALESCE(
-        GROUP_CONCAT(${Diagnoses.name}, ' + '), 
-        '<غير محدد>'
-      )`.as('diagnosis'),
-    })
-    .from(Patients)
-    .leftJoin(People, eq(Patients.person_id, People.id))
-    .leftJoin(Wards, eq(Patients.recent_ward, Wards.id))
-    .leftJoin(Patient_Diagnoses, eq(Patients.id, Patient_Diagnoses.patient_id))
-    .leftJoin(Diagnoses, eq(Diagnoses.id, Patient_Diagnoses.diagnosis_id))
-    .where(isNull(Patients.discharge_date))
-    .orderBy(Patients.admission_date)
-    .groupBy(Patients.id);
-
+    .select()
+    .from(inPatient_view)
+    .where(isNull(inPatient_view.discharge_time));
   return json(allPatients);
 }
